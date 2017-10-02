091aa953f73ec87efb4682d14ed049584c955c01
thuviengithub0208 <khoaphp@yahoo.com>
2017-10-02 19:08:25 +0700

    Initial commit

---
+++ app/src/main/java/com/ptp/phamtanphat/webservice0208/MainActivity.java
@@ -0,0 +1,41 @@
+package com.ptp.phamtanphat.webservice0208;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        readData("http://localhost/webservice/data.php");
+    }
+
+    private void readData(String url) {
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("BBB",error.getMessage().toString());
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+    }
+}
